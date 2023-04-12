6d86f7c8c80165df8181566318b952fc43ea025c
seuli <seulime@gmail.com>
2023-04-12 21:02:29 +0900

    First Commit

---
+++ src/test/java/com/seulime/preprojecttutorial/PreProjectTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seulime.preprojecttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
